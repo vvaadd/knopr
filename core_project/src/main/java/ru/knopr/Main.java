c55c68d0be6ef080eebeef602dd9d39df385fd55
vadim <rybinvv@gmail.com>
2014-06-09 00:51:56 +0400

    rename delete trash

---
+++ core_project/src/main/java/ru/knopr/Main.java
@@ -0,0 +1,14 @@
+package ru.knopr;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        LOG.info("Main method begins");
+        System.out.println("Hello!");
+        LOG.info("Main method ends");
+    }
+}
--- main_project/src/main/java/ru/knopr/Main.java
+++
@@ -1,7 +0,0 @@
-package ru.knopr;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello!");
-    }
-}
